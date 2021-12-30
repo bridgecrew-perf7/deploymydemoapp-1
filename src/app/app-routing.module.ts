668504b6e87f8386d51f6ccf4dc565ca66215357
Josimar <josimar85209@gmail.com>
2021-12-30 11:16:29 -0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AutomationComponent } from './automation/automation.component';
+import { DataScienceComponent } from './data-science/data-science.component';
+import { ProfileComponent } from './profile/profile.component';
+const routes: Routes = [
+{path:'automation', component: AutomationComponent},
+{path:'data-science', component: DataScienceComponent},
+{path:'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { ProfileComponent } from './profile/profile.component';
+import { DataScienceComponent } from './data-science/data-science.component';
+import { AutomationComponent } from './automation/automation.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    DataScienceComponent,
+    AutomationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
